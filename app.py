ffe3fda51ce34c4dba25fe8e66355f42ddfb5f3c
smit022 <133737158+smit022@users.noreply.github.com>
2023-05-16 10:37:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -48,7 +48,6 @@ options_acc_area = ['Other', 'Office areas', 'Residential areas', ' Church areas
 # features list
 features = ['Number_of_vehicles_involved','Number_of_casualties','Hour_of_Day','Type_of_collision','Age_band_of_driver','Sex_of_driver',
     'Educational_level','Service_year_of_vehicle','Day_of_week','Area_accident_occured']
-
 # Give a title to web app using html syntax
 st.markdown("<h1 style='text-align: center;'>Accident Severity Prediction App 🚧</h1>", unsafe_allow_html=True)
 
@@ -98,30 +97,31 @@ def main():
        [Kaggle](https://www.kaggle.com/avikumart) 
        """)
 
-        a, b, c = st.columns([0.2, 0.6, 0.2])
-        with b:
-            st.image("banner-picture.jpeg", use_column_width=True)
 
-        # description about the project and code files
-        st.subheader("🧾Description:")
-        st.text("""This data set is collected from Addis Ababa Sub-city police departments for master's research work. 
-        The data set has been prepared from manual records of road traffic accidents of the year 2017-20. 
-        All the sensitive information has been excluded during data encoding and finally it has 32 features and 12316 instances of the accident.
-        Then it is preprocessed and for identification of major causes of the accident by analyzing it using different machine learning classification algorithms.
-        """)
+a, b, c = st.columns([0.2, 0.6, 0.2])
+with b:
+    st.image("banner-picture.jpeg", use_column_width=True)
 
-        st.markdown(
-            "Source of the dataset: [Click Here](https://www.narcis.nl/dataset/RecordID/oai%3Aeasy.dans.knaw.nl%3Aeasy-dataset%3A191591)")
+# description about the project and code files
+st.subheader("🧾Description:")
+st.text("""This data set is collected from Addis Ababa Sub-city police departments for master's research work. 
+The data set has been prepared from manual records of road traffic accidents of the year 2017-20. 
+All the sensitive information has been excluded during data encoding and finally it has 32 features and 12316 instances of the accident.
+Then it is preprocessed and for identification of major causes of the accident by analyzing it using different machine learning classification algorithms.
+""")
 
-        st.subheader("🧭 Problem Statement:")
-        st.text("""The target feature is Accident_severity which is a multi-class variable. 
-        The task is to classify this variable based on the other 31 features step-by-step by going through each day's task. 
-        The metric for evaluation will be f1-score
-        """)
+st.markdown(
+    "Source of the dataset: [Click Here](https://www.narcis.nl/dataset/RecordID/oai%3Aeasy.dans.knaw.nl%3Aeasy-dataset%3A191591)")
 
-        st.markdown(
-            "Please find GitHub repository link of project: [Click Here](https://github.com/avikumart/Road-Traffic-Severity-Classification-Project)")
+st.subheader("🧭 Problem Statement:")
+st.text("""The target feature is Accident_severity which is a multi-class variable. 
+The task is to classify this variable based on the other 31 features step-by-step by going through each day's task. 
+The metric for evaluation will be f1-score
+""")
 
-        # run the main function
-        if __name__ == '__main__':
-            main()
+st.markdown(
+    "Please find GitHub repository link of project: [Click Here](https://github.com/avikumart/Road-Traffic-Severity-Classification-Project)")
+
+# run the main function
+if __name__ == '__main__':
+    main()
